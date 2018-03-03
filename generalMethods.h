de88ee95b3702aa73326aad7fcb9c2385ebe4add
Tarun Kumar Yadav <tkyiitd@gmail.com>
2018-03-03 15:54:44 +0530

    moved definition from main.h to generalMethods

--- generalMethods.h
+++ generalMethods.h
@@ -3,7 +3,7 @@
 #include "structs.h"
 
 namespace generalMethods{
-		//-----------------methods of vertices--------------------------
+	//-----------------methods of vertices--------------------------
 
 	// sort the vertices along a given edge
 	std::vector<vertex2D> sortVertices(std::vector<vertex2D> V, edge2D e);
@@ -37,6 +37,14 @@ namespace generalMethods{
 	// takes a plane all edges on a plane (found using findEdgesOnPlane) 
 	// and returns all the vertices on that plane 
 	std::vector<vertex3D> findVerticesOnPlane (plane p, edgesOnPlane eop);	
+
+	// check if plane hides an edge from direction
+	bool checkHidden(plane p , edge3D e , edge3D direction) ; // still trying to formulate the probelem
+	// HOW TO CHECK HIDDEN :
+	// first check both end points of edge lie on same side of plane substitute the take product and multiply result should be greater than 0
+	// take an obnoxiously large coordinate in the direction you want to see like if you are observing from +y direction put (0,INF,0) where INF is large number
+	// take a point and return if substitution product less than 0
+
 }
 
 #endif
