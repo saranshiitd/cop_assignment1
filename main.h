44a8934f1df83f83d3b1ed36c19ba5188cdcc5f2
saranshiitd <saranshverma79@gmail.com>
2018-03-03 15:20:26 +0530

    check proection

--- main.h
+++ main.h
@@ -3,3 +3,7 @@ bool checkOverlapCollinear(edge3D e1, edge3D e2) ;
 
 // check edges collinear
 bool checkCoplanar(edge3D e1, edge3D e2 , edge3D e3) ;
+
+// check if plane hides an edge from direction
+bool checkHidden(plane p , edge3D e , edge3D direction) ; // still trying to formulate the probelem
+
