1c0352131eb4f35f7419da53f31fc118a3d0125a
Tarun Kumar Yadav <tkyiitd@gmail.com>
2018-03-11 12:25:11 +0530

    remvoed funciton body from structs

--- structs.h
+++ structs.h
@@ -107,21 +107,11 @@ struct vertexEdgeList
 //////////////////////////////////////////////////////////////////////////////////////////
 
 //! helping methods
-float dotProduct(float vector1[] , float vector2[] ) {
-	return vector1[0]*vector2[0] + vector1[1]*vector2[1] + vector1[2]*vector2[2] ; 
-}
+float dotProduct(float vector1[] , float vector2[] );
 
-float *crossProduct(float vector1[] , float vector2[]) {
-	float *product = new float[3] ;
-	product[0] = vector1[1]*vector2[2] - vector1[2]*vector2[1] ; 
-	product[1] = vector1[2]*vector2[0] - vector1[0]*vector2[2] ; 
-	product[2] = vector1[0]*vector2[1] - vector1[1]*vector2[0] ; 
-	return product ; 
-}
+float *crossProduct(float vector1[] , float vector2[]);
 
-float magnitude(float vector1[]) {
-	return vector1[0]*vector1[0] + vector1[1]*vector1[1] + vector1[2]*vector1[2] ;  
-}
+float magnitude(float vector1[]);
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 
