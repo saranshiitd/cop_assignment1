311dce0c8091069836b156e408e6e9d7f9e6e1a6
Tarun Kumar Yadav <tkyiitd@gmail.com>
2018-03-13 13:57:22 +0530

    checking confinment

--- main.cpp
+++ main.cpp
@@ -25,44 +25,76 @@ using namespace std;
 int main(){
 
 //************************* 3rd example ************************//
-// cube
+// hollow cube
 	VertexList2D frontProjVertex;
 	vertex2D tempVertex2D1;
 	vertex2D tempVertex2D2;
 	vertex2D tempVertex2D3;
 	vertex2D tempVertex2D4;
+	vertex2D tempVertex2D5;
+	vertex2D tempVertex2D6;
+	vertex2D tempVertex2D7;
+	vertex2D tempVertex2D8;
 
-	tempVertex2D1.a = 0; tempVertex2D1.b = 0;
+	tempVertex2D1.a = 0; tempVertex2D1.b = 0; // 0,0
 	frontProjVertex.addVertex(tempVertex2D1);
 
-	tempVertex2D2.a = 0; tempVertex2D2.b = 1;
+	tempVertex2D2.a = 0; tempVertex2D2.b = 1; // 0,1
 	frontProjVertex.addVertex(tempVertex2D2);
 
-	tempVertex2D3.a = 1; tempVertex2D3.b = 0;
+	tempVertex2D3.a = 1; tempVertex2D3.b = 0; // 1,0
 	frontProjVertex.addVertex(tempVertex2D3);
 
-	tempVertex2D4.a = 1; tempVertex2D4.b = 1;
+	tempVertex2D4.a = 1; tempVertex2D4.b = 1; // 1,1
+	frontProjVertex.addVertex(tempVertex2D4);
+
+	tempVertex2D5.a = 0.25; tempVertex2D5.b = 0; // 0.25,0
+	frontProjVertex.addVertex(tempVertex2D4);
+
+	tempVertex2D6.a = 0.75; tempVertex2D6.b = 0; // 0.75,0
+	frontProjVertex.addVertex(tempVertex2D4);
+
+	tempVertex2D7.a = 0.25; tempVertex2D7.b = 1; // 0.25,1
+	frontProjVertex.addVertex(tempVertex2D4);
+
+	tempVertex2D8.a = 0.75; tempVertex2D8.b = 1; // 0.75,1
 	frontProjVertex.addVertex(tempVertex2D4);
 
 	EdgeList2D frontProjEdges;
 	frontProjEdges.addEdge (tempVertex2D1, tempVertex2D2);
-	frontProjEdges.addEdge (tempVertex2D2, tempVertex2D4);
-	frontProjEdges.addEdge (tempVertex2D1, tempVertex2D3);
+	frontProjEdges.addEdge (tempVertex2D1, tempVertex2D5);
+	frontProjEdges.addEdge (tempVertex2D5, tempVertex2D6);
+	frontProjEdges.addEdge (tempVertex2D6, tempVertex2D3);
+	frontProjEdges.addEdge (tempVertex2D2, tempVertex2D7);
+	frontProjEdges.addEdge (tempVertex2D7, tempVertex2D8);
+	frontProjEdges.addEdge (tempVertex2D8, tempVertex2D4);
 	frontProjEdges.addEdge (tempVertex2D3, tempVertex2D4);
 
 /****************/
 	VertexList2D topProjVertex;
-	tempVertex2D1.a = 0; tempVertex2D1.b = 0;
+	tempVertex2D1.a = 0; tempVertex2D1.b = 0; // 0,0
 	topProjVertex.addVertex(tempVertex2D1);
 
-	tempVertex2D2.a = 0; tempVertex2D2.b = 1;
+	tempVertex2D2.a = 0; tempVertex2D2.b = 1; // 0,1
 	topProjVertex.addVertex(tempVertex2D2);
 
-	tempVertex2D3.a = 1; tempVertex2D3.b = 0;
+	tempVertex2D3.a = 1; tempVertex2D3.b = 0; // 1,0
 	topProjVertex.addVertex(tempVertex2D3);
 
-	tempVertex2D4.a = 1; tempVertex2D4.b = 1;
+	tempVertex2D4.a = 1; tempVertex2D4.b = 1; // 1,1
 	topProjVertex.addVertex(tempVertex2D4);
+
+	tempVertex2D5.a = 0.25; tempVertex2D5.b = 0.25; // 0.25,0.25
+	frontProjVertex.addVertex(tempVertex2D4);
+
+	tempVertex2D6.a = 0.25; tempVertex2D6.b = 0.75; // 0.25,0.75
+	frontProjVertex.addVertex(tempVertex2D4);
+
+	tempVertex2D7.a = 0.75; tempVertex2D7.b = 0.25; // 0.75,0.25
+	frontProjVertex.addVertex(tempVertex2D4);
+
+	tempVertex2D8.a = 0.75; tempVertex2D8.b = 0.75; // 0.75,0.75
+	frontProjVertex.addVertex(tempVertex2D4);
 
 	EdgeList2D topProjEdges;
 	topProjEdges.addEdge (tempVertex2D1, tempVertex2D2);
@@ -70,24 +102,45 @@ int main(){
 	topProjEdges.addEdge (tempVertex2D1, tempVertex2D3);
 	topProjEdges.addEdge (tempVertex2D3, tempVertex2D4);
 
+	topProjEdges.addEdge (tempVertex2D5, tempVertex2D6);
+	topProjEdges.addEdge (tempVertex2D5, tempVertex2D7);
+	topProjEdges.addEdge (tempVertex2D6, tempVertex2D8);
+	topProjEdges.addEdge (tempVertex2D7, tempVertex2D8);
+
 /****************/
 	VertexList2D sideProjVertex;
-	tempVertex2D1.a = 0; tempVertex2D1.b = 0;
+	tempVertex2D1.a = 0; tempVertex2D1.b = 0; // 0,0
 	sideProjVertex.addVertex(tempVertex2D1);
 
-	tempVertex2D2.a = 0; tempVertex2D2.b = 1;
+	tempVertex2D2.a = 0; tempVertex2D2.b = 1; // 0,1
 	sideProjVertex.addVertex(tempVertex2D2);
 
-	tempVertex2D3.a = 1; tempVertex2D3.b = 0;
+	tempVertex2D3.a = 1; tempVertex2D3.b = 0; // 1,0
 	sideProjVertex.addVertex(tempVertex2D3);
 
-	tempVertex2D4.a = 1; tempVertex2D4.b = 1;
+	tempVertex2D4.a = 1; tempVertex2D4.b = 1; // 1,1
+	sideProjVertex.addVertex(tempVertex2D4);
+
+	tempVertex2D5.a = 0.25; tempVertex2D5.b = 0; // 0.25,0
+	sideProjVertex.addVertex(tempVertex2D4);
+
+	tempVertex2D6.a = 0.75; tempVertex2D6.b = 0; // 0.75,0
+	sideProjVertex.addVertex(tempVertex2D4);
+
+	tempVertex2D7.a = 0.25; tempVertex2D7.b = 1; // 0.25,1
+	sideProjVertex.addVertex(tempVertex2D4);
+
+	tempVertex2D8.a = 0.75; tempVertex2D8.b = 1; // 0.75,1
 	sideProjVertex.addVertex(tempVertex2D4);
 
 	EdgeList2D sideProjEdges;
 	sideProjEdges.addEdge (tempVertex2D1, tempVertex2D2);
-	sideProjEdges.addEdge (tempVertex2D2, tempVertex2D4);
-	sideProjEdges.addEdge (tempVertex2D1, tempVertex2D3);
+	sideProjEdges.addEdge (tempVertex2D1, tempVertex2D5);
+	sideProjEdges.addEdge (tempVertex2D5, tempVertex2D6);
+	sideProjEdges.addEdge (tempVertex2D6, tempVertex2D3);
+	sideProjEdges.addEdge (tempVertex2D2, tempVertex2D7);
+	sideProjEdges.addEdge (tempVertex2D7, tempVertex2D8);
+	sideProjEdges.addEdge (tempVertex2D8, tempVertex2D4);
 	sideProjEdges.addEdge (tempVertex2D3, tempVertex2D4);
 
 /****************/
@@ -131,12 +184,12 @@ int main(){
 
 
 
-	cout << "First LOOp:"<<"\n";
-	generalMethods::printEdgeList(tempBasicLoopEdgeSet.at(0).at(0).eList);cout<<"\n";
-	cout << "Second LOOp:"<<"\n";
-	generalMethods::printEdgeList(tempBasicLoopEdgeSet.at(1).at(0).eList);cout<<"\n";
-	int confi = generalMethods::checkConfinement(tempBasicLoopEdgeSet.at(0).at(0), tempBasicLoopEdgeSet.at(1).at(0));
-	cout << "Confi: " << confi;
+	// cout << "First LOOp:"<<"\n";
+	// generalMethods::printEdgeList(tempBasicLoopEdgeSet.at(0).at(0).eList);cout<<"\n";
+	// cout << "Second LOOp:"<<"\n";
+	// generalMethods::printEdgeList(tempBasicLoopEdgeSet.at(1).at(0).eList);cout<<"\n";
+	// int confi = generalMethods::checkConfinement(tempBasicLoopEdgeSet.at(0).at(0), tempBasicLoopEdgeSet.at(1).at(0));
+	// cout << "Confi: " << confi;
 
 	return 0;
 }
